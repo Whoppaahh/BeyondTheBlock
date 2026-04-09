a2aa8b5f7d3a7e4ee76423dcc96b715925673847
Whoppaahh <wh0pper28@hotmail.com>
2026-04-09 23:37:10 +0100

    Signs + 1

--- src/main/java/net/ryan/beyond_the_block/client/render/ClientRenderRegistrar.java
+++ src/main/java/net/ryan/beyond_the_block/client/render/ClientRenderRegistrar.java
@@ -5,14 +5,12 @@ import net.ryan.beyond_the_block.client.render.colour.ColourProviderRegistrar;
 import net.ryan.beyond_the_block.client.render.entity.EntityRenderRegistrar;
 import net.ryan.beyond_the_block.client.render.layer.BlockRenderLayerRegistrar;
 import net.ryan.beyond_the_block.client.render.light.DynamicLightRegistrar;
-import net.ryan.beyond_the_block.client.render.sign.ModSignRenderLayers;
 
 public class ClientRenderRegistrar {
 
     public static void register(){
         BlockEntityRenderRegistrar.register();
         EntityRenderRegistrar.register();
-        ModSignRenderLayers.register();
         BlockRenderLayerRegistrar.register();
         DynamicLightRegistrar.register();
         ColourProviderRegistrar.register();

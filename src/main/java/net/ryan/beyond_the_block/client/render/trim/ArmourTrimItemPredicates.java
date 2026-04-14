4842fdf792313ebca668e7dc614dca1c8de73815
Whoppaahh <wh0pper28@hotmail.com>
2026-04-14 22:58:37 +0100

    Armour trims are working correctly now, time to implement the remaining armours

--- src/main/java/net/ryan/beyond_the_block/client/render/trim/ArmourTrimItemPredicates.java
+++ src/main/java/net/ryan/beyond_the_block/client/render/trim/ArmourTrimItemPredicates.java
@@ -4,96 +4,70 @@ import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredica
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
-import net.ryan.beyond_the_block.content.registry.ModItems;
-import net.ryan.beyond_the_block.content.registry.ModTrimRegistry;
 import net.ryan.beyond_the_block.content.registry.family.ModArmourTrim;
-import net.ryan.beyond_the_block.core.BeyondTheBlock;
 
 public final class ArmourTrimItemPredicates {
-
-    private static final Identifier TRIM_PATTERN = new Identifier(BeyondTheBlock.MOD_ID, "trim_pattern");
 
     private ArmourTrimItemPredicates() {
     }
 
     public static void register() {
-        // Vanilla armor
-        registerTrimPredicates(Items.LEATHER_HELMET);
-        registerTrimPredicates(Items.LEATHER_CHESTPLATE);
-        registerTrimPredicates(Items.LEATHER_LEGGINGS);
-        registerTrimPredicates(Items.LEATHER_BOOTS);
+        registerVanillaArmor(Items.IRON_HELMET);
+        registerVanillaArmor(Items.IRON_CHESTPLATE);
+        registerVanillaArmor(Items.IRON_LEGGINGS);
+        registerVanillaArmor(Items.IRON_BOOTS);
 
-        registerTrimPredicates(Items.CHAINMAIL_HELMET);
-        registerTrimPredicates(Items.CHAINMAIL_CHESTPLATE);
-        registerTrimPredicates(Items.CHAINMAIL_LEGGINGS);
-        registerTrimPredicates(Items.CHAINMAIL_BOOTS);
+        registerVanillaArmor(Items.DIAMOND_HELMET);
+        registerVanillaArmor(Items.DIAMOND_CHESTPLATE);
+        registerVanillaArmor(Items.DIAMOND_LEGGINGS);
+        registerVanillaArmor(Items.DIAMOND_BOOTS);
 
-        registerTrimPredicates(Items.IRON_HELMET);
-        registerTrimPredicates(Items.IRON_CHESTPLATE);
-        registerTrimPredicates(Items.IRON_LEGGINGS);
-        registerTrimPredicates(Items.IRON_BOOTS);
+        registerVanillaArmor(Items.GOLDEN_HELMET);
+        registerVanillaArmor(Items.GOLDEN_CHESTPLATE);
+        registerVanillaArmor(Items.GOLDEN_LEGGINGS);
+        registerVanillaArmor(Items.GOLDEN_BOOTS);
 
-        registerTrimPredicates(Items.GOLDEN_HELMET);
-        registerTrimPredicates(Items.GOLDEN_CHESTPLATE);
-        registerTrimPredicates(Items.GOLDEN_LEGGINGS);
-        registerTrimPredicates(Items.GOLDEN_BOOTS);
+        registerVanillaArmor(Items.NETHERITE_HELMET);
+        registerVanillaArmor(Items.NETHERITE_CHESTPLATE);
+        registerVanillaArmor(Items.NETHERITE_LEGGINGS);
+        registerVanillaArmor(Items.NETHERITE_BOOTS);
 
-        registerTrimPredicates(Items.DIAMOND_HELMET);
-        registerTrimPredicates(Items.DIAMOND_CHESTPLATE);
-        registerTrimPredicates(Items.DIAMOND_LEGGINGS);
-        registerTrimPredicates(Items.DIAMOND_BOOTS);
+        registerVanillaArmor(Items.CHAINMAIL_HELMET);
+        registerVanillaArmor(Items.CHAINMAIL_CHESTPLATE);
+        registerVanillaArmor(Items.CHAINMAIL_LEGGINGS);
+        registerVanillaArmor(Items.CHAINMAIL_BOOTS);
 
-        registerTrimPredicates(Items.NETHERITE_HELMET);
-        registerTrimPredicates(Items.NETHERITE_CHESTPLATE);
-        registerTrimPredicates(Items.NETHERITE_LEGGINGS);
-        registerTrimPredicates(Items.NETHERITE_BOOTS);
-
-        // Modded armor
-        registerTrimPredicates(ModItems.RUBY_HELMET);
-        registerTrimPredicates(ModItems.RUBY_CHESTPLATE);
-        registerTrimPredicates(ModItems.RUBY_LEGGINGS);
-        registerTrimPredicates(ModItems.RUBY_BOOTS);
-
-        registerTrimPredicates(ModItems.NOCTURNITE_HELMET);
-        registerTrimPredicates(ModItems.NOCTURNITE_CHESTPLATE);
-        registerTrimPredicates(ModItems.NOCTURNITE_LEGGINGS);
-        registerTrimPredicates(ModItems.NOCTURNITE_BOOTS);
-
-        registerTrimPredicates(ModItems.AZUROS_HELMET);
-        registerTrimPredicates(ModItems.AZUROS_CHESTPLATE);
-        registerTrimPredicates(ModItems.AZUROS_LEGGINGS);
-        registerTrimPredicates(ModItems.AZUROS_BOOTS);
-
-        registerTrimPredicates(ModItems.AMBERINE_HELMET);
-        registerTrimPredicates(ModItems.AMBERINE_CHESTPLATE);
-        registerTrimPredicates(ModItems.AMBERINE_LEGGINGS);
-        registerTrimPredicates(ModItems.AMBERINE_BOOTS);
-
-        registerTrimPredicates(ModItems.MIRANITE_HELMET);
-        registerTrimPredicates(ModItems.MIRANITE_CHESTPLATE);
-        registerTrimPredicates(ModItems.MIRANITE_LEGGINGS);
-        registerTrimPredicates(ModItems.MIRANITE_BOOTS);
-
-        registerTrimPredicates(ModItems.CHROMITE_HELMET);
-        registerTrimPredicates(ModItems.CHROMITE_CHESTPLATE);
-        registerTrimPredicates(ModItems.CHROMITE_LEGGINGS);
-        registerTrimPredicates(ModItems.CHROMITE_BOOTS);
-
-        registerTrimPredicates(ModItems.ROSETTE_HELMET);
-        registerTrimPredicates(ModItems.ROSETTE_CHESTPLATE);
-        registerTrimPredicates(ModItems.ROSETTE_LEGGINGS);
-        registerTrimPredicates(ModItems.ROSETTE_BOOTS);
-
-        registerTrimPredicates(ModItems.XIRION_HELMET);
-        registerTrimPredicates(ModItems.XIRION_CHESTPLATE);
-        registerTrimPredicates(ModItems.XIRION_LEGGINGS);
-        registerTrimPredicates(ModItems.XIRION_BOOTS);
+        registerVanillaArmor(Items.LEATHER_HELMET);
+        registerVanillaArmor(Items.LEATHER_CHESTPLATE);
+        registerVanillaArmor(Items.LEATHER_LEGGINGS);
+        registerVanillaArmor(Items.LEATHER_BOOTS);
     }
 
-    private static void registerTrimPredicates(Item item) {
-        FabricModelPredicateProviderRegistry.register(item, TRIM_PATTERN, (stack, world, entity, seed) ->
-                ModArmourTrim.getTrim(stack)
-                        .map(trim -> (float) ModTrimRegistry.getPatternIndex(trim.patternId()))
+    private static void registerVanillaArmor(Item item) {
+        registerPatternPredicate(item, "coast");
+        registerPatternPredicate(item, "dune");
+        registerPatternPredicate(item, "eye");
+        registerPatternPredicate(item, "host");
+        registerPatternPredicate(item, "raiser");
+        registerPatternPredicate(item, "rib");
+        registerPatternPredicate(item, "sentry");
+        registerPatternPredicate(item, "shaper");
+        registerPatternPredicate(item, "silence");
+        registerPatternPredicate(item, "snout");
+        registerPatternPredicate(item, "spire");
+        registerPatternPredicate(item, "tide");
+        registerPatternPredicate(item, "vex");
+        registerPatternPredicate(item, "ward");
+        registerPatternPredicate(item, "wayfinder");
+        registerPatternPredicate(item, "wild");
+    }
+
+    private static void registerPatternPredicate(Item item, String patternPath) {
+        FabricModelPredicateProviderRegistry.register(
+                item,
+                new Identifier("beyond_the_block", "trim_" + patternPath),
+                (stack, world, entity, seed) -> ModArmourTrim.getTrim(stack)
+                        .map(trim -> trim.patternId().getPath().equals(patternPath) ? 1.0F : 0.0F)
                         .orElse(0.0F)
         );
     }
